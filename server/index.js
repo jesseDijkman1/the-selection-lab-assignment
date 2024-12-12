fdfd1fb45b5f02dda42d4b7dd401e87dea5c2630
Jesse Dijkman <jesse.dijkman@live.nl>
2024-12-12 14:00:02 +0100

    chore: Add dotenv & make first call to spoonacular

--- server/index.js
+++ server/index.js
@@ -2,14 +2,27 @@
  * - Proxy API (connects to the recipe API and returns transformed data)
  * - Stores saved recipes in a fake db
  */
-
+require("dotenv").config();
 const express = require("express");
 
 const PORT = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", async (req, res) => {
+  const headers = new Headers();
+  headers.append("x-api-key", process.env.API_KEY);
+  headers.append("Content-Type", "application/json");
+
+  try {
+    const response = await fetch("https://api.spoonacular.com/recipes/random", {
+      headers,
+    });
+    const json = await response.json();
+
+    res.json(json);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 app.listen(PORT, () => {
