1fd4eb9ef97a95e234e9939c013d28d5e71f2f48
Jesse Dijkman <jesse.dijkman@live.nl>
2024-12-16 12:21:10 +0100

    chore: Update BEM helper to support chaining & rename some props to help minifier

--- client/src/js/lib/BEM.ts
+++ client/src/js/lib/BEM.ts
@@ -1,4 +1,29 @@
 class BEM {
+  b: string;
+  c: string[]; // Stack
+
+  constructor(baseClass: string) {
+    this.b = baseClass;
+    this.c = [];
+  }
+
+  p(str: string) {
+    this.c.push(str);
+    return this;
+  }
+
+  // This one is probably a bit much ... and maybe confusing?
+  clearChainIf(condition: boolean) {
+    if (condition) this.c = [];
+    return this;
+  }
+
+  toString() {
+    const className = this.c.join(" ");
+    this.c = [];
+    return className;
+  }
+
   static createTransformer = (variationClass: string) => (baseClass: string) =>
     `${baseClass}--${variationClass}`;
 
@@ -7,6 +32,35 @@ class BEM {
   static EMPTY = BEM.createTransformer("empty");
   static NO_RESULTS = BEM.createTransformer("no-results");
   static STATIC = BEM.createTransformer("static");
+
+  get RAW() {
+    return this.p(this.b);
+  }
+
+  get ACTIVE() {
+    return this.p(BEM.ACTIVE(this.b));
+  }
+
+  get OPEN() {
+    return this.p(BEM.OPEN(this.b));
+  }
+
+  get EMPTY() {
+    return this.p(BEM.EMPTY(this.b));
+  }
+
+  get NO_RESULTS() {
+    return this.p(BEM.NO_RESULTS(this.b));
+  }
+
+  get STATIC() {
+    return this.p(BEM.STATIC(this.b));
+  }
+
+  *[Symbol.iterator]() {
+    let cls;
+    while ((cls = this.c.shift())) yield cls;
+  }
 }
 
 export default BEM;
